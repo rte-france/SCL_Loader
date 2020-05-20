917b850fc079ada32d57c63602bc03d105ec1539
GilGuillet <64019625+GilGuillet@users.noreply.github.com>
2020-05-20 09:52:04 +0200

    Added licence
    Investigation on pyQT5 / Treeview

--- TreeView.py
+++ TreeView.py
@@ -1,3 +1,14 @@
+#
+# Copyright (c) 2019-2020, RTE (https://www.rte-france.com)
+# See AUTHORS.txt
+#
+# This Source Code Form is subject to the terms of the Apache License, version 2.0.
+# If a copy of the Apache License, version 2.0 was not distributed with this file,
+# you can obtain one at http://www.apache.org/licenses/LICENSE-2.0.
+# SPDX-License-Identifier: Apache-2.0
+#
+# This file is part of [R#SPACE], [IEC61850 Digital Contronl System testing.
+#
 
 from IEC61850_XML_Class import IED
 
