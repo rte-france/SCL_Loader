70317bc9edb85b46e9118b4b7b04802c90a88e28
Pascal Senizergues <62218568+Synx-FR@users.noreply.github.com>
2021-11-18 16:24:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ with open(path.join(HERE, 'requirements.txt'), encoding='utf-8') as f:
 # Fields marked as "Optional" may be commented out.
 setup(
     name='scl_loader',  # Required
-    version='1.9.2',  # Required
+    version='1.9.3',  # Required
     description='Outil de manipulation de SCD',  # Required
     long_description=LONG_DESCRIPTION,  # Optional
     long_description_content_type='text/markdown',  # Optional (see note above)
